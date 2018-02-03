b94a5582c59ed3d9ff7c5f9a9b791eb08ec3ed0c
konradhalas <halas.konrad@gmail.com>
2018-02-03 16:21:41 +0100

    Improve talks app

--- mysite/talks/models.py
+++ mysite/talks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import CASCADE
+from django.utils.text import slugify
 
 LANGUAGES = (
     ('polish', 'Polish'),
@@ -7,10 +8,24 @@ LANGUAGES = (
 )
 
 
+def talk_slides_upload(instance, filename):
+    extension = filename.split('.')[-1]
+    new_filename = slugify(instance.title)
+    return f'slides/{new_filename}.{extension}'
+
+
 class Talk(models.Model):
     title = models.CharField(max_length=256)
-    description = models.TextField()
-    slides = models.FileField()
+    description = models.TextField(blank=True, null=True)
+    slides = models.FileField(upload_to=talk_slides_upload, null=True, blank=True)
+
+    def start_year(self):
+        event = self.events.last()
+        return event.date.year if event else None
+
+    def end_year(self):
+        event = self.events.first()
+        return event.date.year if event else None
 
     def __str__(self):
         return self.title
@@ -23,6 +38,9 @@ class Event(models.Model):
     video_url = models.URLField(blank=True, null=True)
     date = models.DateField()
     language = models.CharField(choices=LANGUAGES, max_length=16)
+
+    class Meta:
+        ordering = ['-date']
 
     def __str__(self):
         return self.name
--- mysite/talks/views.py
+++ mysite/talks/views.py
@@ -1,10 +1,13 @@
+from django.db.models import Max
 from django.shortcuts import render
 
 from talks.models import Talk
 
 
 def talks_list_view(request):
-    talks = Talk.objects.all()
+    talks = Talk.objects.annotate(
+        last_event_date=Max('events__date'),
+    ).order_by('-last_event_date').prefetch_related('events')
     return render(
         request=request,
         template_name='talks/talks_list.html',
