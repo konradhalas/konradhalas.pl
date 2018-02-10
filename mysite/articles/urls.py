11568c6d7dab2c1797c0670304d79156e571f01e
konradhalas <halas.konrad@gmail.com>
2018-02-10 21:40:44 +0100

    Missing slash

--- mysite/articles/urls.py
+++ mysite/articles/urls.py
@@ -4,5 +4,5 @@ from articles import views
 
 urlpatterns = [
     path('', views.articles_list_view, name='articles_list'),
-    path('<str:slug>', views.article_details_view, name='article_details'),
+    path('<str:slug>/', views.article_details_view, name='article_details'),
 ]
