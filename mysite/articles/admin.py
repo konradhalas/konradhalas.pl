0375c1ee366761e9a1867dda362d1a5a3286609a
konradhalas <halas.konrad@gmail.com>
2018-02-17 13:48:57 +0100

    Prepopulate slug field

--- mysite/articles/admin.py
+++ mysite/articles/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from articles.models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Article, ArticleAdmin)
