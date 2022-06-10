fac503ec4314ee0f6d8113fc19b1fc21d18b25da
konradhalas <halas.konrad@gmail.com>
2022-06-10 09:46:44 +0200

    nginx-proxy updates

--- containers/run.sh
+++ containers/run.sh
@@ -1,4 +1,4 @@
 ../containers/wait-for-it.sh -t 60 db:5432
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --socket :8000 --module mysite.wsgi --processes 4 --threads 2
+uwsgi --http :$VIRTUAL_PORT --module mysite.wsgi --processes 4 --threads 2
