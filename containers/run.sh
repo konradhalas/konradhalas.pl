fbb8d0a1f2f0c70126c674c66ca31679bcab9160
konradhalas <halas.konrad@gmail.com>
2023-02-06 21:39:50 +0100

    Fix static/media path

--- containers/run.sh
+++ containers/run.sh
@@ -1,4 +1,4 @@
 ../containers/wait-for-it.sh -t 60 db:5432
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --http :$VIRTUAL_PORT --module mysite.wsgi --processes 4 --threads 2
+uwsgi --http :$VIRTUAL_PORT --module mysite.wsgi --static-map /static=/static --static-map /media=/media --processes 4 --threads 2
